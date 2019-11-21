cccfa6d702ea8ddf30c22e3c901f17e8e4943303
RESCOBAR UAO <e_lopez1996@hotmail.com>
2019-11-20 23:08:36 -0500

    se limpia el template y se acomodan las medidas del html y body

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './views/index.vue'
 
 export default {
   name: 'app',
@@ -17,12 +14,11 @@ export default {
 </script>
 
 <style>
-#app {
+html,body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #1D1A26;
+  color: #ffffff;
+  height: 100vh;
+  margin: 0;
 }
 </style>
