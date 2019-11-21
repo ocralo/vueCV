cd41c2d3b7c6f37cea2824b6cdfb8388341e3d44
RESCOBAR UAO <e_lopez1996@hotmail.com>
2019-11-21 01:35:39 -0500

    se termina la vista de conoceme

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mt-5">
     <div class="row justify-content-around">
-      <div class="col-md-3 rel-bg-content rel-rounded shadow p-4">
+      <div class="col-md-3 rel-bg-content rel-bg-content-left rel-rounded shadow p-4">
         <div class="rel-img-profile shadow"></div>
         <div
           class="rel-info-one text-center d-flex flex-column align-items-center justify-content-center"
@@ -40,22 +40,168 @@
           </div>
         </div>
       </div>
-      <div class="col-md-8 rel-bg-content rel-rounded shadow">
-        <div class="row justify-content-around">
-          <div class="col-md-3">
-            <h4>Conoceme</h4>
-          </div>
-          <div class="col-md-9 bg-primary rel-nav">
-            <button type="button" class="btn btn-primary">Conoceme</button>
-            <button type="button" class="btn btn-primary">Proyectos</button>
-            <button type="button" class="btn btn-primary">Habilidades</button>
-            <button type="button" class="btn btn-primary">Estudios</button>
-            <button type="button" class="btn btn-primary">Certificados</button>
+      <div class="col-md-8 rel-bg-content rel-bg-content-rigth rel-rounded shadow">
+        <div class="row d-flex justify-content-end">
+          <div class="col-md-8 bg-primary rel-nav">
+            <button type="button" class="btn btn-primary h-100">Conoceme</button>
+            <button type="button" class="btn btn-primary h-100">Proyectos</button>
+            <button type="button" class="btn btn-primary h-100">Estudios</button>
           </div>
         </div>
-        <div class="row">
+        <div class="row mt-4">
           <div class="container-fluid">
-            <div class="row"></div>
+            <div class="row">
+              <div class="col-md-12 pb-3">
+                <h3 class="rel-title-menu">Conoceme</h3>
+                <hr class="rel-hr-title" />
+                <p
+                  class="rel-paragraph"
+                >Estudiante de Ingeniería Multimedia, con experiencia en la creación y edición de contenidos para sitios web, desarrollo de aplicaciones y trabajo en equipo. Manejo programas como Adobe Illustrator, Photoshop, Premiere Pro, wordpress; tambien lenguajes de programación, tales como html, css, javascript, c# y java.Sin embargo, cuento con la capacidad de acoplarme facilmente a un nuevo lenguaje o programa de desarrollo. Por otro lado, soy una persona entusiasta, apasionado y pro activa para las tareas que me son asignadas. Cuento con la habilidad para encontrar la solución a problemas que se presenten de forma rápida y efectiva.</p>
+                <h4 class="mt-5">Intereses</h4>
+                <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
+                  <div class="rel-item-interest d-flex m-1">
+                    <i class="fas fa-gamepad mr-3 rel-icon"></i>
+                    <p class="m-0 p-0">Videojuegos</p>
+                  </div>
+                  <div class="rel-item-interest d-flex m-1">
+                    <i class="fas fa-music mr-3 rel-icon"></i>
+                    <p class="m-0 p-0">Musica</p>
+                  </div>
+                  <div class="rel-item-interest d-flex m-1">
+                    <i class="fas fa-tv mr-3 rel-icon"></i>
+                    <p class="m-0 p-0">Anime</p>
+                  </div>
+                  <div class="rel-item-interest d-flex m-1">
+                    <i class="fas fa-book-open mr-3 rel-icon"></i>
+                    <p class="m-0 p-0">DC Animated</p>
+                  </div>
+                </div>
+                <h4 class="mt-4">Habilidades</h4>
+                <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">Wordpress</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 40%"
+                        aria-valuenow="40"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >40%</div>
+                    </div>
+                  </div>
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">Java</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 40%"
+                        aria-valuenow="40"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >60%</div>
+                    </div>
+                  </div>
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">MYSQL</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 60%"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >60%</div>
+                    </div>
+                  </div>
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">JavaScript</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 70%"
+                        aria-valuenow="70"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >70%</div>
+                    </div>
+                  </div>
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">Unity</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 60%"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >60%</div>
+                    </div>
+                  </div>
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">React</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 65%"
+                        aria-valuenow="65"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >65%</div>
+                    </div>
+                  </div>
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">Vue</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 40%"
+                        aria-valuenow="40"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >40%</div>
+                    </div>
+                  </div>
+                </div>
+                <h4 class="mt-4">Idiomas</h4>
+                <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">Español (natal)</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 90%"
+                        aria-valuenow="90"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >90%</div>
+                    </div>
+                  </div>
+                  <div class="rel-item-hability m-1 w-md-25">
+                    <p class="m-0 p-0">Ingles</p>
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 40%"
+                        aria-valuenow="40"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >40%</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-12"></div>
+            </div>
           </div>
         </div>
       </div>
@@ -70,6 +216,13 @@ export default {};
 <style>
 .rel-bg-content {
   background: #232130;
+  border-radius: 1em;
+}
+.rel-bg-content-left {
+  max-height: 42em;
+}
+.rel-bg-content-2 {
+  background: #211e2b;
   border-radius: 1em;
 }
 .rel-img-profile {
@@ -99,7 +252,35 @@ export default {};
   width: 18em;
 }
 .rel-nav {
-  border-radius: 0 1em 0em 1em;
+  border-radius: 0em 1em 0em 1em;
+}
+
+.rel-icon {
+  color: #273bdd;
+  font-size: 1.5em;
+}
+
+.rel-hr-title {
+  background-color: #273bdd;
+  height: 6px;
+  border-radius: 1em;
+  width: 4em;
+  margin: 0em 0em 2em 0em;
+}
+.rel-paragraph {
+  line-height: 1.3;
+}
+
+.rel-item-hability {
+  width: 45%;
+}
+
+.progress-bar {
+  background-color: #273bdd;
+}
+
+#barra-wordpress:hover:before {
+  content: "50%";
 }
 
 @media (max-width: 992px) {
@@ -116,6 +297,9 @@ export default {};
     margin-right: auto;
     height: 10em;
     width: 10em;
+  }
+  .rel-nav {
+    border-radius: 1em 1em 0em 0em;
   }
 }
 </style>
