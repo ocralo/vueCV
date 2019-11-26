573de3654fcd35a606cf200988efa57fb0568c46
RESCOBAR UAO <e_lopez1996@hotmail.com>
2019-11-26 13:04:53 -0500

    se crean las vistas

---
+++ src/views/AboutMe.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="row">
+    <div class="col-md-12 pb-3">
+      <h3 class="rel-title-menu">Conoceme</h3>
+      <hr class="rel-hr-title" />
+      <p
+        class="rel-paragraph"
+      >Estudiante de Ingeniería Multimedia, con experiencia en la creación y edición de contenidos para sitios web, desarrollo de aplicaciones y trabajo en equipo. Manejo programas como Adobe Illustrator, Photoshop, Premiere Pro, wordpress; tambien lenguajes de programación, tales como html, css, javascript, c# y java.Sin embargo, cuento con la capacidad de acoplarme facilmente a un nuevo lenguaje o programa de desarrollo. Por otro lado, soy una persona entusiasta, apasionado y pro activa para las tareas que me son asignadas. Cuento con la habilidad para encontrar la solución a problemas que se presenten de forma rápida y efectiva.</p>
+      <h4 class="mt-5">Intereses</h4>
+      <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
+        <div class="rel-item-interest d-flex m-1">
+          <i class="fas fa-gamepad mr-3 rel-icon"></i>
+          <p class="m-0 p-0">Videojuegos</p>
+        </div>
+        <div class="rel-item-interest d-flex m-1">
+          <i class="fas fa-music mr-3 rel-icon"></i>
+          <p class="m-0 p-0">Musica</p>
+        </div>
+        <div class="rel-item-interest d-flex m-1">
+          <i class="fas fa-tv mr-3 rel-icon"></i>
+          <p class="m-0 p-0">Anime</p>
+        </div>
+        <div class="rel-item-interest d-flex m-1">
+          <i class="fas fa-book-open mr-3 rel-icon"></i>
+          <p class="m-0 p-0">DC Animated</p>
+        </div>
+      </div>
+      <h4 class="mt-4">Habilidades</h4>
+      <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">Wordpress</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 40%"
+              aria-valuenow="40"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >40%</div>
+          </div>
+        </div>
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">Java</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 40%"
+              aria-valuenow="40"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >60%</div>
+          </div>
+        </div>
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">MYSQL</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 60%"
+              aria-valuenow="60"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >60%</div>
+          </div>
+        </div>
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">JavaScript</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 70%"
+              aria-valuenow="70"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >70%</div>
+          </div>
+        </div>
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">Unity</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 60%"
+              aria-valuenow="60"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >60%</div>
+          </div>
+        </div>
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">React</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 65%"
+              aria-valuenow="65"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >65%</div>
+          </div>
+        </div>
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">Vue</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 40%"
+              aria-valuenow="40"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >40%</div>
+          </div>
+        </div>
+      </div>
+      <h4 class="mt-4">Idiomas</h4>
+      <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">Español (natal)</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 90%"
+              aria-valuenow="90"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >90%</div>
+          </div>
+        </div>
+        <div class="rel-item-hability m-1 w-md-25">
+          <p class="m-0 p-0">Ingles</p>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 40%"
+              aria-valuenow="40"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >40%</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+  components: {}
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Proyect.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="row">
+    <div class="col-md-12 pb-3">
+      <h3 class="rel-title-menu">Proyectos</h3>
+      <hr class="rel-hr-title" />
+      <div class="container-fluid">
+        <div class="row justify-content-around">
+          <div class="col-md-5 rel-bg-content-2 mt-4  d-flex flex-md-wrap flex-lg-nowrap  justify-content-around pt-3 pb-4">
+            <div class="rel-tex-icon-proyect pr-2">
+              <i class="fab fa-java"></i>
+            </div>
+            <div class="">
+              <h5 class="p-3">Instacol</h5>
+              <p
+                class="rel-paragraph-proyect text-white-50 text-justify"
+              >Proyecto realizado en Java y MYSQL, en el que se hace una aproximación de la aplicación instagram.</p>
+              <a
+                class="mb-1 p-2"
+                href="https://github.com/ocralo/Instacol"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Ver repositorio</a>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 mt-4  d-flex flex-md-wrap flex-lg-nowrap  justify-content-around pt-3 pb-4">
+            <div class="rel-tex-icon-proyect pr-2">
+              <i class="fab fa-wordpress-simple"></i>
+            </div>
+            <div class="">
+              <h5 class="p-3">Pagina Web Semillero De Animación</h5>
+              <p
+                class="rel-paragraph-proyect text-white-50 text-justify"
+              >Proyecto realizado en Wordpress, en el que se desarrolló una página web para el semillero de animacion de la Universidad Autónoma de Occidente.</p>
+              <a
+                class="mb-1 p-2"
+                href="https://www.uao.edu.co/seanima/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Ver Pagina</a>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 mt-4  d-flex flex-md-wrap flex-lg-nowrap  justify-content-around pt-3 pb-4">
+            <div class="rel-tex-icon-proyect pr-2">
+              <i class="fab fa-wordpress-simple"></i>
+            </div>
+            <div class="">
+              <h5 class="p-3">UAO LABS</h5>
+              <p
+                class="rel-paragraph-proyect text-white-50 text-justify"
+              >Proyecto realizado en Wordpress, en el que se desarrolló una página web para los laboratorios de la Universidad Autónoma de Occidente.</p>
+              <a
+                class="mb-1 p-2"
+                href="https://sitios.uao.edu.co/labs/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Ver Pagina</a>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 mt-4  d-flex flex-md-wrap flex-lg-nowrap  justify-content-around pt-3 pb-4">
+            <div class="rel-tex-icon-proyect pr-2">
+              <i class="fab fa-vuejs"></i>
+            </div>
+            <div class="">
+              <h5 class="p-3">Fincasenalquiler.co</h5>
+              <p
+                class="rel-paragraph-proyect text-white-50 text-justify"
+              >Desarrolló una página web, con vista cliente y vista administrador, en la cual se puede observar en la vista cliente las diferentes fincas que se pueden alquilar y en la vista administrador se desarrolló un crud para los datos que se mostrarán en la vista cliente.</p>
+              <a
+                class="mb-1 p-2"
+                href="http://fincasenalquiler.co/#/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Ver Pagina</a>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 mt-4  d-flex flex-md-wrap flex-lg-nowrap  justify-content-around pt-3 pb-4">
+            <div class="rel-tex-icon-proyect pr-2">
+              <i class="fab fa-react"></i>
+            </div>
+            <div class="">
+              <h5 class="p-3">IMUAO</h5>
+              <p
+                class="rel-paragraph-proyect text-white-50 text-justify"
+              >Proyecto que busca visualizar los perfiles y proyectos de los estudiantes de ingeniería multimedia de la Universidad Autónoma de Occidente.</p>
+              <a
+                class="mb-1 p-2"
+                href="https://github.com/ocralo/imuao"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Ver repositorio</a>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 mt-4  d-flex flex-md-wrap flex-lg-nowrap  justify-content-around pt-3 pb-4">
+            <div class="rel-tex-icon-proyect pr-2">
+              <i class="fab fa-vuejs"></i>
+            </div>
+            <div class="">
+              <h5 class="p-3">Chatbot UAO Virtual</h5>
+              <p
+                class="rel-paragraph-proyect text-white-50 text-justify"
+              >Desarrollo de un Chatbot que facilite el manejo de la plataforma UAO Virtual y solucione las dudas que tengan sobre el manejo de la plataforma</p>
+              <a
+                href="https:pcampus.uaovirtual.edu.co/login/index.php"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Ver Pagina</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style>
+</style>
