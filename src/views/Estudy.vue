b9406308eb09d703dbd0d8c8f350af826249377c
RESCOBAR UAO <e_lopez1996@hotmail.com>
2019-11-26 13:53:35 -0500

    se termina la vista de estudios

--- src/views/Estudy.vue
+++ src/views/Estudy.vue
@@ -3,159 +3,119 @@
     <div class="col-md-12 pb-3">
       <h3 class="rel-title-menu">Estudios</h3>
       <hr class="rel-hr-title" />
-      <p
-        class="rel-paragraph"
-      >Estudiante de Ingeniería Multimedia, con experiencia en la creación y edición de contenidos para sitios web, desarrollo de aplicaciones y trabajo en equipo. Manejo programas como Adobe Illustrator, Photoshop, Premiere Pro, wordpress; tambien lenguajes de programación, tales como html, css, javascript, c# y java.Sin embargo, cuento con la capacidad de acoplarme facilmente a un nuevo lenguaje o programa de desarrollo. Por otro lado, soy una persona entusiasta, apasionado y pro activa para las tareas que me son asignadas. Cuento con la habilidad para encontrar la solución a problemas que se presenten de forma rápida y efectiva.</p>
-      <h4 class="mt-5">Intereses</h4>
-      <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
-        <div class="rel-item-interest d-flex m-1">
-          <i class="fas fa-gamepad mr-3 rel-icon"></i>
-          <p class="m-0 p-0">Videojuegos</p>
-        </div>
-        <div class="rel-item-interest d-flex m-1">
-          <i class="fas fa-music mr-3 rel-icon"></i>
-          <p class="m-0 p-0">Musica</p>
-        </div>
-        <div class="rel-item-interest d-flex m-1">
-          <i class="fas fa-tv mr-3 rel-icon"></i>
-          <p class="m-0 p-0">Anime</p>
-        </div>
-        <div class="rel-item-interest d-flex m-1">
-          <i class="fas fa-book-open mr-3 rel-icon"></i>
-          <p class="m-0 p-0">DC Animated</p>
-        </div>
-      </div>
-      <h4 class="mt-4">Habilidades</h4>
-      <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">Wordpress</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 40%"
-              aria-valuenow="40"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >40%</div>
+      <h4 class="mt-5">Educación</h4>
+      <div class="container-fluid">
+        <div class="row justify-content-between pl-4 pr-4">
+          <div class="col-md-5 rel-bg-content-2 p-3">
+            <div class="d-flex">
+              <i class="fas fa-user-cog rel-icon mr-3"></i>
+              <div>
+                <h5 class="mb-0">Ingeniero Multimedia</h5>
+                <p class="text-secondary mb-3 mt-0">Enero 2013 - Junio 2019</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Se está realizando en la Universidad Autónoma de Occidente en la ciudad de Santiago de Cali con prevista obtención del título en el año 2020.</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">Java</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 40%"
-              aria-valuenow="40"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >60%</div>
+          <div class="col-md-5 rel-bg-content-2 p-3">
+            <div class="d-flex">
+              <i class="fas fa-user-graduate rel-icon mr-3"></i>
+              <div>
+                <h5 class="mb-0">Técnico En Instalaciones Eléctricas Residenciales</h5>
+                <p class="text-secondary mb-3 mt-0">Enero 2010 - Junio 2013</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Realizado en el Colegio Comfandi el Prado de la ciudad de Santiago de Cali con obtención del título en el año 2013.</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">MYSQL</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 60%"
-              aria-valuenow="60"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >60%</div>
-          </div>
-        </div>
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">JavaScript</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 70%"
-              aria-valuenow="70"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >70%</div>
-          </div>
-        </div>
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">Unity</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 60%"
-              aria-valuenow="60"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >60%</div>
-          </div>
-        </div>
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">React</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 65%"
-              aria-valuenow="65"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >65%</div>
-          </div>
-        </div>
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">Vue</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 40%"
-              aria-valuenow="40"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >40%</div>
+          <div class="col-md-5 rel-bg-content-2 p-3 mt-md-5">
+            <div class="d-flex">
+              <i class="fas fa-user-graduate rel-icon mr-3"></i>
+              <div>
+                <h5 class="mb-0">Bachiller</h5>
+                <p class="text-secondary mb-3 mt-0">Enero 2007 - Junio 2013</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Realizado en el Colegio Comfandi el Prado de la ciudad de Santiago de Cali con obtención del titulo en el año 2013.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <h4 class="mt-4">Idiomas</h4>
-      <div class="d-flex flex-wrap justify-content-around pt-4 pb-4 rel-bg-content-2">
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">Español (natal)</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 90%"
-              aria-valuenow="90"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >90%</div>
+      <h4 class="mt-4">Certificados</h4>
+      <div class="container-fluid">
+        <div class="row justify-content-between pl-4 pr-4">
+          <div class="col-md-5 rel-bg-content-2 p-3">
+            <div class="d-flex">
+              <i class="fas fa-award rel-icon mr-3"></i>
+              <div class="div">
+                <h5 class="mb-0">Taller Para Monitores En Recreación</h5>
+                <p class="text-secondary mb-3 mt-0">Noviembre 2017</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Certificado por Moncho Recreo Recreación, Nit: 900.144.426-7, Santiago de Cali.</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="rel-item-hability m-1 w-md-25">
-          <p class="m-0 p-0">Ingles</p>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 40%"
-              aria-valuenow="40"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >40%</div>
+          <div class="col-md-5 rel-bg-content-2 p-3">
+            <div class="d-flex">
+              <i class="fas fa-award rel-icon mr-3"></i>
+              <div class="div">
+                <h5 class="mb-0">Escuela De Liderazgo Promocion XXXII</h5>
+                <p class="text-secondary mb-3 mt-0">Junio 2015</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Certificado por Bienestar Universitario, Universidad Autonoma de Occidente, Santiago de Cali.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 p-3 mt-5">
+            <div class="d-flex">
+              <i class="fas fa-award rel-icon mr-3"></i>
+              <div class="div">
+                <h5 class="mb-0">SQL Fundamentals Course</h5>
+                <p class="text-secondary mb-3 mt-0">Septiembre 2017</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Certificado por SOLOLEARN, certificado #1060-6081380</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 p-3 mt-5">
+            <div class="d-flex">
+              <i class="fas fa-award rel-icon mr-3"></i>
+              <div class="div">
+                <h5 class="mb-0">Java Tutorial Course</h5>
+                <p class="text-secondary mb-3 mt-0">Junio 2018</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Certificado por SOLOLEARN, certificado #1068-6081380</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5 rel-bg-content-2 p-3 mt-5">
+            <div class="d-flex">
+              <i class="fas fa-award rel-icon mr-3"></i>
+              <div class="div">
+                <h5 class="mb-0">JavaScript Tutorial Course</h5>
+                <p class="text-secondary mb-3 mt-0">Mayo 2018</p>
+                <p
+                  class="rel-paragraph-proyect text-white-50 text-justify"
+                >Certificado por SOLOLEARN, certificado #1024-6081380</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-md-12"></div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Estudy',
+  name: "Estudy",
   components: {}
 };
 </script>
